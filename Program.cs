805f4ca4419142a50d8d4d95d4ed95c6b8e2dabd
“Gomolemo <gomomohapi@gmail.com>
2024-04-10 10:58:06 +0200

    Update Program.cs file

--- Program.cs
+++ Program.cs
@@ -16,29 +16,13 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
+app.MapGet("/GenerateSocialMediaPost/{podcastUrl}", (string podcastUrl) =>
 {
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
+    return PodcastAppAPI.PodcastCopilot.GenerateSocialMediaPost(podcastUrl);
 })
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+    .WithName("GetSocialMediaPost")
+    .WithSummary("Generate Social Media Post")
+    .WithDescription("Generates a blurb / social media post with an image for a podcast episode based on the podcast url provided.")
+    .WithOpenApi();
 
 app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
